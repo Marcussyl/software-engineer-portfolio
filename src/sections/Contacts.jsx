6b9c64ed901ab7919be1e1337d6d4d53d32fb1fd
marcus.sze <szeyulam1028@gmail.com>
2025-05-28 22:53:22 +0800

    feat:add tilted card animation for contact form

--- src/sections/Contacts.jsx
+++ src/sections/Contacts.jsx
@@ -1,61 +1,117 @@
-import { motion } from "motion/react";
+import { motion, useSpring, useTransform, useMotionValue } from "motion/react";
 
 export const Contacts = () => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const mouseXSpring = useSpring(x);
+  const mouseYSpring = useSpring(y);
+
+  const rotateX = useTransform(mouseYSpring, [-0.5, 0.5], ["7.5deg", "-7.5deg"]);
+  const rotateY = useTransform(mouseXSpring, [-0.5, 0.5], ["-7.5deg", "7.5deg"]);
+
+  const handleMouseMove = (e) => {
+    const rect = e.target.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const xPct = mouseX / width - 0.5;
+    const yPct = mouseY / height - 0.5;
+    console.log({xPct, yPct});
+
+    x.set(xPct);
+    y.set(yPct);
+  }
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  }
+
   return (
-    <div className="flex flex-col items-center pt-10">
-      {/* <h1 className="section-title gradient-text">Get in touch with me</h1> */}
-      {/* <p className="section-desc"></p> */}
-      <div
-        class="flex flex-col items-center px-10 pt-5 pb-10 m-5 mx-auto rounded-lg border-2 border-dashed border-main-purple max-w-[500px]"
+    <div className="flex flex-col items-center pt-10 m-5">
+      <motion.div
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        class="transform-3d flex flex-col items-center px-10 pt-5 pb-10 mx-auto rounded-lg border-2 border-dashed border-main-purple max-w-[500px]"
         style={{
+          rotateX,
+          rotateY,
           background:
             "linear-gradient(to bottom right, #261a42 0%, #1e0e3b 20%, #140534 50%, #1e0e3d 80%, #281c44 90%, #281c44 100%)",
         }}
       >
-        <h2 class="gradient-text font-bold text-2xl">Get in touch</h2>
-        <br />
-        <form action="https://example.com/submit" class="">
-          <label for="name">Name</label>
-          <div class="input-wrapper">
-            <input id="name" type="text" placeholder="Chan Tai Man" />
-          </div>
+        <div className="inner transform-3d translate-z-[75px]">
+          <h2 class="gradient-text font-bold text-2xl text-center">
+            Get in touch
+          </h2>
           <br />
-          <label for="email">Email</label>
-          <div class="input-wrapper">
-            <input id="email" type="email" placeholder="1234@example.com" />
-          </div>
-          <br />
-          <label for="msg">Message</label>
-          <div class="input-wrapper">
-            <textarea
-              id="msg"
-              name="msg"
-              rows="4"
-              cols="50"
-              placeholder="Write your message here"
-            ></textarea>
-          </div>
-          <br />
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.9 }}
-            className="glowing-btn block"
-            type="submit"
-            class="glowing-btn block mx-auto w-full rounded-[10px]"
-          >
-            <div class="inner justify-center rounded-[10px]">
-              Submit message
-              <img src="/assets/mail.svg" alt="mail icon" />
-              <img src="/assets/plane.svg" alt="plane icon" />
+          <form action="https://example.com/submit" class="">
+            <label for="name">Name</label>
+            <div class="input-wrapper">
+              <input id="name" type="text" placeholder="Chan Tai Man" />
             </div>
-          </motion.button>
-        </form>
-        <div className="social-medias-container">
-          <a href=""><img src="/assets/social-media/facebook.png" alt="" /></a>
-          <a href=""><img src="/assets/social-media/github.png" alt="" /></a>
-          <a href=""><img src="/assets/social-media/linkedin.png" alt="" /></a>
+            <br />
+            <label for="email">Email</label>
+            <div class="input-wrapper">
+              <input id="email" type="email" placeholder="1234@example.com" />
+            </div>
+            <br />
+            <label for="msg">Message</label>
+            <div class="input-wrapper">
+              <textarea
+                id="msg"
+                name="msg"
+                rows="4"
+                cols="50"
+                placeholder="Write your message here"
+              ></textarea>
+            </div>
+            <br />
+            <button
+              // whileHover={{ scale: 1.05 }}
+              // whileTap={{ scale: 0.9 }}
+              className="glowing-btn block mx-auto w-full rounded-[10px] cursor-pointer"
+              type="submit"
+              // class="glowing-btn block mx-auto w-full rounded-[10px] !cursor-pointer"
+            >
+              <div class="inner justify-center rounded-[10px] !cursor-pointer">
+                Submit message
+                <img src="/assets/mail.svg" alt="mail icon" />
+                <img src="/assets/plane.svg" alt="plane icon" />
+              </div>
+            </button>
+            {/* <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.9 }}
+              className="glowing-btn block w-full cursor-pointer"
+              type="submit"
+              // class="glowing-btn block mx-auto w-full rounded-[10px] !cursor-pointer"
+            >
+              <div class="inner justify-center rounded-[10px] !cursor-pointer">
+                Submit message
+                <img src="/assets/mail.svg" alt="mail icon" />
+                <img src="/assets/plane.svg" alt="plane icon" />
+              </div>
+            </motion.button> */}
+          </form>
+          <div className="social-medias-container">
+            <a href="">
+              <img src="/assets/social-media/facebook.png" alt="" />
+            </a>
+            <a href="">
+              <img src="/assets/social-media/github.png" alt="" />
+            </a>
+            <a href="">
+              <img src="/assets/social-media/linkedin.png" alt="" />
+            </a>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
