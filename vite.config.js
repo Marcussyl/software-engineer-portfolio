36e0f4b5a8abaf1a9f63d21041040514fa047382
Marcus.sze <marcus.sze@jervoisventures.comntures.com>
2025-06-10 12:37:24 +0800

    chore: update base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mars",
+  base: "/software-engineer-portfolio",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
