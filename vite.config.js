6ee1c6d46c982011d6a7b0f1eb5941997b829a4e
Marcus.sze <marcus.sze@jervoisventures.comntures.com>
2025-06-10 12:21:50 +0800

    feat: add github action and github page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/mars",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
